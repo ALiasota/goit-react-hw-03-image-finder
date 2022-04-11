8a9ba6aa46c0e93cee85b3927d1cfe0b2a91fbf3
Andrii <Andrii@iMac.local>
2022-04-11 18:40:19 +0300

    sewqe

--- src/servises/images-api.js
+++ src/servises/images-api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const key = '25783532-c25c49afce5183be9881181c4';
+// const key = '25783532-c25c49afce5183be9881181c4';
 
 const fetchImages = ({searchQuery='', currentPage=1, pageSize=12})=>{
     return axios
         .get(
-            `https://pixabay.com/api/?q=${searchQuery}&key=${key}&page=${currentPage}&image_type=photo&orientation=horizontal&per_page=${pageSize}`
+            `https://pixabay.com/api/?q=${searchQuery}&key=25783532-c25c49afce5183be9881181c4&page=${currentPage}&image_type=photo&orientation=horizontal&per_page=${pageSize}`
             )
         .then(response => response.data);
 }
